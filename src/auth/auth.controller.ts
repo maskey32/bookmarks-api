bd086c198580b75ecc900a7d2e59ad5b4ad631f0
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-01-31 22:55:34 +0100

    chore: Setup auth controller and added signup and signin functions with their logics

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthContoller {
+    constructor(private authService: AuthService) {}
+
+        @Post('signup')
+        signup(@Body() dto: AuthDto) {
+            console.log({ dto, });
+            
+            return this.authService.signup(dto);
+        }
+
+        @Post('signin')
+        signin(@Body() dto: AuthDto) {
+            console.log({ dto });
+            
+            return this.authService.signin(dto);
+        }
+    
+}
